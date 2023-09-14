ea602845a3d045e2afd06401ece5c473767691ed
julicarlomagno <julicarlomagno@gmail.com>
2023-09-14 10:56:40 -0300

    dashboard de clase

---
+++ src/constants/index.js
@@ -0,0 +1,29 @@
+
+
+export const sideBar = [
+  {
+    id: "categorias",
+    title: "Categorias",
+    route: "/categorias"
+  },
+  {
+    id: "lastUser",
+    title: "Ultimo Usuario",
+    route: "/lastUser"
+  },
+  {
+    id: "dataUsers",
+    title: "Usuarios",
+    route: "/tableUser" 
+  },
+  {
+    id: "dataProducts",
+    title: "Productos",
+    route: "/tableProducts"
+  },
+  {
+    id: "searchMovies",
+    title: "Search",
+    route: "/searchmovies"
+  },
+]
